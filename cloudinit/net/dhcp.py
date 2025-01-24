10bb8df176c8b67e13a00a288e65f993d1bb84f3
ubuntu <ubuntu@ip-10-0-1-88>
2025-01-24 20:01:21 +0000

    waleko__gpt-4o_e69e__id_26

--- cloudinit/net/dhcp.py
+++ cloudinit/net/dhcp.py
@@ -177,7 +177,7 @@ class DhcpClient(abc.ABC):
 
     @classmethod
     def kill_dhcp_client(cls):
-        subp.subp(["pkill", cls.client_name], rcs=[0, 1])
+        subp.subp(["pkill", cls.client_name], rcs=[0, 1], capture=True)
 
     @classmethod
     def clear_leases(cls):
