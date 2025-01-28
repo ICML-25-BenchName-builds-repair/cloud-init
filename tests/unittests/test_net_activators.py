a9b056fa5be5e986042ff78732817dcd75b402ed
ubuntu <ubuntu@ip-10-0-1-29>
2025-01-28 14:13:26 +0000

    waleko__gemini-pro-1.5_7054__id_25

--- tests/unittests/test_net_activators.py
+++ tests/unittests/test_net_activators.py
@@ -327,7 +327,7 @@ class TestActivatorsBringDown:
 
 class TestNetworkManagerActivatorBringUp:
     def fake_isfile_no_nmconn(filename):
-        return False if filename.endswith(".nmconnection") else True
+        return False if ".nmconnection" in filename else True
 
     @patch("cloudinit.subp.subp", return_value=("", ""))
     @patch(
