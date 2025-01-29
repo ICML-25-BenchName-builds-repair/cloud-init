3078bc62ba82ebb68805da4fd831723dd80aba9a
ubuntu <ubuntu@ip-10-0-1-114>
2025-01-29 11:45:07 +0000

    waleko__claude-3.5-sonnet_d353__id_29

--- cloudinit/distros/__init__.py
+++ cloudinit/distros/__init__.py
@@ -112,11 +112,11 @@ LDH_ASCII_CHARS = string.ascii_letters + string.digits + "-"
 # This also isn't exhaustive. If you have a unique case that adheres to
 # the `packages` schema, you can add it here.
 PackageList = Union[
-    List[str],
-    List[Mapping],
-    List[List[str]],
+    List[str],  
+    List[Mapping[Any, Any]],
+    List[List[str]], 
     List[Union[str, List[str]]],
-    List[Union[str, List[str], Mapping]],
+    List[Union[str, List[str], Mapping[Any, Any]]],
 ]
 
 
